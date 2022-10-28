53926d061839ead82b3cb32b85e00c899e2b340d
Jefferson Monteiro <jefferson.willian.monteiro@gmail.com>
2022-10-27 23:33:06 -0400

    Add main logic to schedule a slot time.

--- app.ts
+++ app.ts
@@ -1,9 +1,36 @@
-import {Browser} from './lib/browser';
+import { Browser } from './lib/browser';
+import { getDates } from './lib/dates';
+import * as urlHelper from './lib/url';
+import { Interactor } from './lib/interactor';
+
+require('dotenv').config({ path: __dirname+'/vars.env' });
 
 (async () => {
   const browser = new Browser();
+  const interactor = new Interactor(browser);
 
   await browser.initialize();
-  await browser.goto('https://google.com');
+
+  const dates = getDates();
+  for (let i = 0; i < dates.length; ++i) {
+    await browser.goto(urlHelper.getUrl(dates[i]));
+
+    if (!urlHelper.isValid(browser.url())) {
+      // There are no more available dates.
+      break;
+    }
+
+    if (await interactor.maybeChooseSlot() == false) {
+      console.log('No slot available');
+      continue;
+    }
+    console.log('Slot available');
+
+    // Wait for the page to load.
+    await browser.getPage().waitForLoadState();
+
+    await interactor.fillForm();
+  };
+
   await browser.close();
 })();
