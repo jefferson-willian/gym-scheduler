bf04e09cf6ea8047a888b2289add736504867b47
Jefferson Monteiro <jefferson.willian.monteiro@gmail.com>
2022-10-27 23:28:52 -0400

    Add interator that interacts with the web page.

---
+++ lib/interactor.ts
@@ -0,0 +1,33 @@
+import { Browser } from './browser';
+
+export class Interactor {
+  private readonly browser: Browser;
+  private readonly slotTimeLocator: string;
+
+  constructor(browser: Browser) {
+    this.browser = browser;
+    this.slotTimeLocator = 'text=' + process.env.SLOT_TIME!;
+  }
+
+  async maybeChooseSlot(): Promise<boolean> {
+    const locator = this.browser.getPage().locator(this.slotTimeLocator);
+    
+    if (await locator.count() <= 0) {
+      return false;
+    }
+
+    if (await locator.first().isEnabled() == false) {
+      return false;
+    }
+
+    await locator.first().click();
+
+    return true;
+  }
+
+  async fillForm(): Promise<void> {
+    await this.browser.getPage().fill("[id=label1]", process.env.NAME!);
+    await this.browser.getPage().fill("[id=label2]", process.env.APARTMENT!);
+    await this.browser.getPage().fill("[id=label3]", process.env.EMAIL!);
+  }
+}
